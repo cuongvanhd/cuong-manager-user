598c973de886f4e6e526846e1afdc1e799961fbb
Nguyen Van Cuong <cuong@tagbangers.co.jp>
2015-11-24 10:06:15 +0900

    UserControllerTestクラスのテストを更新する。

---
+++ src/test/java/com/springapp/controller/UserControllerTest.java
@@ -0,0 +1,62 @@
+package com.springapp.controller;
+
+import com.springapp.services.UserService;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by Cuong on 2015/11/24.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest extends TestCase {
+
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    UserController userController;
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    ModelMap model;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        MockitoAnnotations.initMocks(this);
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).setViewResolvers(viewResolver).build();
+    }
+
+    @Test
+    public void testShowListUser() throws Exception {
+
+        when(userService.getAllUserInfor()).thenReturn(anyList());
+
+        mockMvc.perform(get("/user/listuser"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("users"))
+                .andExpect(view().name("/user/listuser"));
+
+        verify(userService).getAllUserInfor();
+    }
+}
